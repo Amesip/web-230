307478feb810714a3d4a2d2d00c37549d1ab69dc
Mary Barrett <mkbarrett.1201@gmail.com>
2017-11-28 23:47:39 -0600

    Adding Primitive-types.js

---
+++ week-1/Primitive-types.js
@@ -0,0 +1,28 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Mary";
+var lastName = "Barrett";
+var age = "32";
+var street = "1000 Galvin Road";
+var city = "Bellevue";
+var state = "Nebraska";
+var zipCode = "68005";
+
+// output 
+console.log(''); 
+
+
+// end program 
