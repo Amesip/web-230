2a4210dba616af9aa7be2803d3581c37d0735032
Mary Barrett <mkbarrett.1201@gmail.com>
2017-12-09 22:53:16 -0600

    Adding Barrett-Exercise-2.2

---
+++ week-2/Barrett-Exercise-2.2.js
@@ -0,0 +1,6 @@
+var header = require('Barrett-header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Richard", "Krasso", "Exercise 2.1")); 
+console.log("\n");
---
+++ week-2/Barrett-Exercise-2.3.js
@@ -0,0 +1,34 @@
+var header = require('Barrett-header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    Mary Barrett
+    Exercise 2.3
+    December 9, 2017
+
+    Hello Mary Barrett!
+
+    Hint: Use your personal header display function and
+    refer to page 178 for implementing function properties
+*/ 
+
+// function properties go here...
+
+myName.MaryBarrett
+
+// function 
+function myName() {
+    return myName.MaryBarrett;
+}
+
+// output 
+console.log('\n'); 
+
+
+
+
+
+// end program 
