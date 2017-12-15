3be1abb9093164c121f6d6c80b103c9a0297ae55
Mary Barrett <mkbarrett.1201@gmail.com>
2017-12-14 21:33:24 -0600

    Adding Barrett-Exercise-3.2.js

---
+++ week-3/Barrett-Exercise-3.2.js
@@ -0,0 +1,61 @@
+var header = require('../header.js'); 
+
+// start program
+
+/*
+    Expected output:
+
+    FirstName LastName
+    Exercise 3.2
+    Today's Date
+
+    // output from the match() function 
+    false
+    true 
+
+    // output from the if...else blocks 
+    Truck and Car do not match!
+    Bike and Bike do match!
+    Four and Three do not match!
+*/ 
+
+// functions 
+function match(x) {
+    if (x<3, x>1)
+    return true;
+} else {
+    return false;
+}
+
+function logMismatch("dog") {
+    if not("dog")
+    return "dog and "" do not match!";
+    } 
+
+function logMatch("dog") {
+    if ("dog")
+    return "dog and "" do match!"
+}
+
+var="dog";
+var="cat";
+var="fish";
+var="goat"
+var="lizard"
+var="gerbil"
+
+// six (6) test variables 
+
+
+
+// Output from the match() function... 
+console.log(match("A", "B"));
+console.log(match(2, 2)); 
+
+
+// Conditional "if...else" statements. Include checks for all six (6) test variables 
+if () {
+    logMatch();
+} else {
+    logMismatch();
+}
